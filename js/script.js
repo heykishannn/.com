486bae9cbe77675dff91c9917abac35bf2e9cccf
heykishannn <kishankumar93961@gmail.com>
2025-05-15 12:29:53 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 const downloadButton = document.querySelector('.download-button');
 
 downloadButton.addEventListener('click', function(event) {
-    event.preventDefault(); // डिफ़ॉल्ट क्रिया को रोकें
+    event.preventDefault();
     alert('डाउनलोड शुरू हो रहा है...');
-    // यहाँ आप अपने ऐप के डाउनलोड लिंक पर रीडायरेक्ट कर सकते हैं:
     // window.location.href = 'YOUR_APP_DOWNLOAD_LINK';
 });
