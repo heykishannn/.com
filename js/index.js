50c05a198f7f0047de593f24f80968057cd3860d
heykishannn <kishankumar93961@gmail.com>
2025-05-15 12:10:18 +0530

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// स्टैटिक फ़ाइलों को परोसें (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname)));
+
+// रूट हैंडलर
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
