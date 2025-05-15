3695fcea813465d7263e981d05eb664e9b804e3b
heykishannn <kishankumar93961@gmail.com>
2025-05-15 12:06:18 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+{
+  "name": "nas-website",
+  "version": "1.0.0",
+  "description": "Official website for NAS app",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1"
+  }
+}
